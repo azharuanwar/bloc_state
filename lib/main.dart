df3cdfd6978fbab83fcb0078eaede09d1cdd2cc4
Azharu Anwar <azharu.anwar@gmail.com>
2021-10-22 00:21:42 +0700

    bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_state/color_bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,20 +13,26 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    ColorBloc bloc = ColorBloc();
+
     return MaterialApp(
       home: Scaffold(
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                bloc.eventSink.add(ColorEvent.to_amber);
+              },
               backgroundColor: Colors.amber,
             ),
             const SizedBox(
               width: 10,
             ),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                bloc.eventSink.add(ColorEvent.to_light_blue);
+              },
               backgroundColor: Colors.lightBlue,
             ),
           ],
@@ -34,13 +41,17 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Bloc'),
         ),
         body: Center(
-          child: AnimatedContainer(
-            duration: const Duration(
-              milliseconds: 500,
-            ),
-            width: 100,
-            height: 100,
-            color: Colors.amber,
+          child: StreamBuilder(
+            stream: bloc.stateStream,
+            initialData: Colors.amber,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                width: 100,
+                height: 100,
+                color: snapshot.data,
+              );
+            },
           ),
         ),
       ),
